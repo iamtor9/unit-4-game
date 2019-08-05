0e0276d334d2ec6ebf108584104c776fe7809a70
iamtor9 <iamtor9@gmail.com>
2019-08-05 16:13:31 -0500

    update game.js

---
+++ game.js
@@ -0,0 +1,59 @@
+//player will have to guess the answer, just like in Word Guess. This time, though, the player will guess with numbers instead of letters.
+
+
+let red = 0,
+let y = 1,
+let z = 2,
+let v = 3,
+}
+
+//Here's how the app works:
+
+
+//There will be four crystals displayed as buttons on the page.
+
+<button>crystal1</button>
+<button>crystal2</button>
+<button>crystal3</button>
+<button>crystal4</button>
+
+//The player will be shown a random number at the start of the game.
+
+onclick
+start
+button
+randomNumber
+computerChoice
+
+//When the player clicks on a crystal, it will add a specific amount of points to the player's total score.
+
+//Your game will hide this amount until the player clicks a crystal.
+//When they do click one, update the player's score counter.
+
+
+
+//The player wins if their total score matches the random number from the beginning of the game.
+
+
+//The player loses if their score goes above the random number.
+
+
+//The game restarts whenever the player wins or loses.
+
+//When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
+
+
+
+//The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
+
+
+
+
+
+Option 1 Game design notes
+
+
+The random number shown at the start of the game should be between 19 - 120.
+
+
+Each crystal should have a random hidden value between 1 - 12.
